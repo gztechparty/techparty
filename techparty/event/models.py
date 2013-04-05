00c4848c5f762bc09f187c2ab3843a489e066adf
jeff <jeff@fallever.com>
2013-04-05 11:28:03 +0800

    完善模型

---
+++ techparty/event/models.py
@@ -0,0 +1,56 @@
+#encoding=utf-8
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    slug = models.SlugField(blank=True, null=True)
+    hashtag = models.CharField(max_length=20, blank=True, null=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    sponsor = models.ForeignKey(User, blank=True, null=True)
+    area = models.IntegerField(choices=((0, u'广州'),
+                                        (1, u'深圳'), (2, u'珠海')))
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = u'活动'
+        verbose_name_plural = u'活动'
+
+
+class Participate(models.Model):
+    user = models.ForeignKey(User)
+    event = models.ForeignKey(Event)
+    status = models.IntegerField(choices=((0, u'已报名'), (1, u'已确认'),
+                                          (2, u'已付费'), (3, u'已签到')))
+    signup_time = models.DateTimeField(auto_now_add=True)
+    confirm_time = models.DateTimeField(blank=True, null=True)
+    pay_time = models.DateTimeField(blank=True, null=True)
+    checkin_time = models.DateTimeField(blank=True, null=True)
+
+    def __unicode__(self):
+        return u'%s@%s' % (self.user.first_name, self.event.name)
+
+    class Meta:
+        verbose_name = u'参会纪录'
+        verbose_name_plural = u'参会纪录'
+
+
+class Tweet(models.Model):
+    user = models.ForeignKey(User)
+    event = models.ForeignKey(Event)
+    text = models.CharField(max_length=280, blank=True, null=True)
+    image = models.URLField(blank=True, null=True)
+    add_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.text or self.image
+
+    class Meta:
+        verbose_name = u'直播推文'
+        verbose_name_plural = u'直播推文'
--- techparty/settings.py
+++ techparty/settings.py
@@ -1,34 +1,41 @@
 # Django settings for techparty project.
 
+from sae.const import (MYSQL_HOST, MYSQL_HOST_S,
+    MYSQL_PORT, MYSQL_USER, MYSQL_PASS, MYSQL_DB
+)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('jeff kit', 'jeff@techparty.org'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': MYSQL_DB,                      # Or path to database file if using sqlite3.
+        'USER': MYSQL_USER,                      # Not used with sqlite3.
+        'PASSWORD': MYSQL_PASS,                  # Not used with sqlite3.
+        'HOST': MYSQL_HOST,                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': MYSQL_PORT,                      # Set to empty string for default. Not used with sqlite3.
+    },
+    'OPTIONS': {
+            'init_command': 'SET storage_engine=INNODB',
+    },
 }
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Asia/Shanghai'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-cn'
 
 SITE_ID = 1
 
@@ -115,10 +122,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'techparty.event',
+    'techparty.wechat',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -149,3 +155,9 @@ LOGGING = {
         },
     }
 }
+
+try:
+    #from xsettings import *
+    pass
+except:
+    pass
